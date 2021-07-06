f3cc799b683e16a54d768980172d1831f937e980
thiagocessin <thiagoacessin@gmail.com>
2021-07-06 12:42:42 -0300

    Lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
+@IonicPage()// me permite escrever o nome da página entre aspas
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
