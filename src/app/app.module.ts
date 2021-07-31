4de22d027e77a83206793537719461770923fc1d
thiagocessin <thiagoacessin@gmail.com>
2021-07-31 16:04:17 -0300

    Carregando produtos por categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
+import { ProdutoService } from '../services/domain/produto.service';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { ClienteService } from '../services/domain/cliente.service';
     AuthService,
     StorageService,
     ClienteService,
+    ProdutoService,
   ]
 })
 export class AppModule {}
