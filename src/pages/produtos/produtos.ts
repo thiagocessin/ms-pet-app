710a05431fda1e42dc2b88849a3126909de12f47
thiagocessin <thiagoacessin@gmail.com>
2021-07-31 16:16:38 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../../config/api.config';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -24,9 +25,24 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe((res)=>{
         this.items = res['content'];
+        this.loadImageUrls();
       },
       (error)=>{});
 
   }
+
+  loadImageUrls(){
+    for(var i=0; i < this.items.length;i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(res=>{
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        (error)=>{})
+    }
+
+  }
+
+
 
 }
