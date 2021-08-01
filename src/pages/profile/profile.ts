1b5438a907d8bd8e18e2b10cdd20bc8c7bea420d
thiagocessin <thiagoacessin@gmail.com>
2021-08-01 11:16:18 -0300

    buscando enderecos do DB

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -33,7 +33,7 @@ export class ProfilePage {
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
         .subscribe(res =>{
-          this.cliente = res;
+          this.cliente = res as ClienteDTO;
           this.getImageIfExists()
         },
         (error)=>{
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -13,12 +13,12 @@ export class ClienteService{
 
   }
 
-  findByEmail(email: string) : Observable<ClienteDTO>{
+  findByEmail(email: string) {
 
     //let token = this.storage.getLocalUser().token;
     //let authHeader = new HttpHeaders({'Authorization':'Bearer '+token});
 
-    return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
   }
 
   getImageFromBucket(id: string) :Observable<any>{
